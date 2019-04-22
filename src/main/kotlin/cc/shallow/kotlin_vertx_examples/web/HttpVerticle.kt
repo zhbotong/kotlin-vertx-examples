e5c1a5b4f8c475b83028e6ccb27e60391b4217e6
shenzhou-6 <yizmao.me@gmail.com>
2019-04-22 06:58:29 -0700

    add error handler

--- src/main/kotlin/cc/shallow/kotlin_vertx_examples/web/HttpVerticle.kt
+++ src/main/kotlin/cc/shallow/kotlin_vertx_examples/web/HttpVerticle.kt
@@ -1,5 +1,6 @@
 package cc.shallow.kotlin_vertx_examples.web
 
+import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.jdbc.JDBCClient
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.*
@@ -8,6 +9,8 @@ import io.vertx.kotlin.coroutines.CoroutineVerticle
 
 class HttpVerticle: CoroutineVerticle() {
   private  lateinit var  jdbcClient: JDBCClient
+
+  private val logger = LoggerFactory.getLogger(HttpVerticle::class.java)
 
   override suspend fun start() {
 
@@ -21,6 +24,12 @@ class HttpVerticle: CoroutineVerticle() {
     router.route().handler(LoggerHandler.create(LoggerFormat.DEFAULT))
     //程序处理超时时间
     router.route().handler(TimeoutHandler.create(5000))
+
+    //错误处理
+    router.route().handler(ErrorHandler.create())
+    router.errorHandler(500){
+      logger.error("error.......",it.failure())
+    }
     DemoHandler(vertx,jdbcClient).initRoute("/demo",router)
     vertx.createHttpServer()
       .requestHandler(router)
