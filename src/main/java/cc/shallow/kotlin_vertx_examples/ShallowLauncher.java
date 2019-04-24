79a9267e4a1b7bacfb4d32d8683247494da209b8
yizmao <yizmao.me@gmail.com>
2019-04-24 10:57:54 +0800

    add logback

--- src/main/java/cc/shallow/kotlin_vertx_examples/ShallowLauncher.java
+++ src/main/java/cc/shallow/kotlin_vertx_examples/ShallowLauncher.java
@@ -1,17 +1,26 @@
 package cc.shallow.kotlin_vertx_examples;
 
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import io.netty.util.internal.logging.Slf4JLoggerFactory;
 import io.vertx.core.Launcher;
 import io.vertx.core.VertxOptions;
 
 import static io.vertx.core.spi.resolver.ResolverProvider.DISABLE_DNS_RESOLVER_PROP_NAME;
 
-public class ShallowLauncher extends Launcher {
+public class ShallowLauncher extends Launcher{
   public static void main(String[] args) {
     /**
      * webclient dns_resolver error问题
      * 详见　https://zhuanlan.zhihu.com/p/30913753
      */
     System.getProperties().setProperty(DISABLE_DNS_RESOLVER_PROP_NAME,"true");
+
+    /**
+     * log config see @https://vertx.io/docs/vertx-core/java/#_logging
+     * use logback implement slf4j
+     */
+    InternalLoggerFactory.setDefaultFactory(Slf4JLoggerFactory.INSTANCE);
+    System.getProperties().setProperty("vertx.logger-delegate-factory-class-name","io.vertx.core.logging.SLF4JLogDelegateFactory");
     new ShallowLauncher().dispatch(args);
   }
 
