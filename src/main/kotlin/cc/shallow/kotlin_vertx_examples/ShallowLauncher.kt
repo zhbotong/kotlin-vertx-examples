16e89fff2fcf6258ea20f83d923941b4fb78a30a
shenzhou-6 <yizmao.me@gmail.com>
2019-07-08 17:10:10 +0800

    update kt launcher replace java launcher
    Signed-off-by: weixing <weixing.zhang@phoebusmed.com>

---
+++ src/main/kotlin/cc/shallow/kotlin_vertx_examples/ShallowLauncher.kt
@@ -0,0 +1,49 @@
+package cc.shallow.kotlin_vertx_examples
+
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
+import io.netty.util.internal.logging.InternalLoggerFactory
+import io.netty.util.internal.logging.Slf4JLoggerFactory
+import io.vertx.core.Launcher
+import io.vertx.core.VertxOptions
+import io.vertx.core.json.Json
+import io.vertx.core.spi.resolver.ResolverProvider.DISABLE_DNS_RESOLVER_PROP_NAME
+
+class ShallowLauncher: Launcher() {
+  /**
+   * 代码来自　＠Liu Rui
+   * 项目地址: https://github.com/aruis/vertxstarter/blob/course-4/src/main/java/com/aruistar/vertxstarter/MyLauncher.java
+   * @param options
+   */
+  override fun beforeStartingVertx(options: VertxOptions) {
+    options.warningExceptionTime = 10L * 1000 * 1000000  //block时间超过此值，打印代码堆栈
+    options.blockedThreadCheckInterval = 2000 // 每隔x，检查下是否block
+    options.maxEventLoopExecuteTime = 2L * 1000 * 1000000 //允许eventloop block 的最长时间
+    super.beforeStartingVertx(options)
+  }
+}
+
+fun main(args: Array<String>) {
+  /**
+   * webclient dns_resolver error问题
+   * 详见　https://zhuanlan.zhihu.com/p/30913753
+   */
+  System.getProperties().setProperty(DISABLE_DNS_RESOLVER_PROP_NAME, "true")
+
+  /**
+   * log config see @https://vertx.io/docs/vertx-core/java/#_logging
+   * use logback implement slf4j
+   */
+  InternalLoggerFactory.setDefaultFactory(Slf4JLoggerFactory.INSTANCE)
+  System.getProperties()
+    .setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory")
+
+  /**
+   * add jackson jsr310
+   */
+  Json.mapper.registerModule(ParameterNamesModule())
+    .registerModule(Jdk8Module())
+    .registerModule(JavaTimeModule())
+  ShallowLauncher().dispatch(args)
+}
